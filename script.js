5dda7aa839d0cc16cf14285865ffbd067b9de753
Jairo292 <jairodani@outlook.com>
2026-04-25 23:31:19 -0600

    54534

--- script.js
+++ script.js
@@ -1991,7 +1991,7 @@ function buildModelMapping(countries) {
 			fallbackSrc: defaultFallbackSrc,
 			position: '0 0 -0.5',
 			scale: '80 80 80',
-			scaleMobile: '34 34 34',
+			scaleMobile: '44 44 44',
 			rotateAnim: true,
 		};
 		const override = overridesByName[countryName] || {};
