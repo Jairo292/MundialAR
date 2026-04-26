ffe1561dc414d5e88910b60f58769150bd6890ca
Jairo292 <jairodani@outlook.com>
2026-04-25 23:29:23 -0600

    teateata

--- script.js
+++ script.js
@@ -1991,7 +1991,7 @@ function buildModelMapping(countries) {
 			fallbackSrc: defaultFallbackSrc,
 			position: '0 0 -0.5',
 			scale: '80 80 80',
-			scaleMobile: '24 24 24',
+			scaleMobile: '34 34 34',
 			rotateAnim: true,
 		};
 		const override = overridesByName[countryName] || {};
